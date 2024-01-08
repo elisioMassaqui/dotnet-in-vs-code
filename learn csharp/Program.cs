f71b2bfc7c7ab86e3468395a0aa81244178be50d
Mr.Robot <elisiomassaqui8@gmail.com>
2024-01-08 22:15:10 +0000

    Encontre um método da System.Math classe que retorna o maior de dois números.

--- learn csharp/Program.cs
+++ learn csharp/Program.cs
@@ -7,11 +7,30 @@ int numero = 28;
 Console.WriteLine(numero);
 
 Random dice = new Random();
+//Conjunto de numeros inteiros vai aarmazenar numero aleatorio de 1 a 7
 int roll = dice.Next(1, 7);
 Console.WriteLine(roll);
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
 
+Console.WriteLine($"First Roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
+//Directamente sem atribuir a uma variavel.
+Console.WriteLine(dice.Next(1, 7));
 
+int number = 7;
+string text = "seven";
 
+Console.WriteLine(number);
+Console.WriteLine();
+Console.WriteLine(text);
+
+int firsValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firsValue, secondValue);
+Console.WriteLine(largerValue);
 
 
  // Aguarda a entrada do usuário antes de encerrar o programa.
