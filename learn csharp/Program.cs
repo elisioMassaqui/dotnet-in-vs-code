7cafd49e44f0b4556737eff7d12cba899eb27907
Mr.Robot <elisiomassaqui8@gmail.com>
2024-01-08 20:41:36 +0000

    tudo bem deu pra reverter kkkk

--- learn csharp/Program.cs
+++ learn csharp/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information e voltamos tudo bem kkk
 Console.WriteLine("Hello, World!");
 
 int numero = 28;
--- learn csharp/obj/Debug/net8.0/learn csharp.AssemblyInfo.cs
+++ learn csharp/obj/Debug/net8.0/learn csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learn csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0f6365c38aafa834a30e4b2ee14fcb327b1ee6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("learn csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learn csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
