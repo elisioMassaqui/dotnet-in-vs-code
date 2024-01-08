892d578c6bff7e4695ceb6aa5616bd6a3bf9b3d1
Mr.Robot <elisiomassaqui8@gmail.com>
2024-01-08 20:47:06 +0000

    gitignore

--- learn csharp/Program.cs
+++ learn csharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information e voltamos tudo bem kkk
+//Adicionamos o gitignore pra igonrar coisas que não se devem no visual studio
 Console.WriteLine("Hello, World!");
 
 int numero = 28;
