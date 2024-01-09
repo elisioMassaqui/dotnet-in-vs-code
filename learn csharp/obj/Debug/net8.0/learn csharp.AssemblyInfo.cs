09ae0bdd485aea5784446707d589fd963b03495a
Mr.Robot <elisiomassaqui8@gmail.com>
2024-01-09 17:20:15 +0000

    nulo de novo

--- learn csharp/obj/Debug/net8.0/learn csharp.AssemblyInfo.cs
+++ learn csharp/obj/Debug/net8.0/learn csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learn csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4478cb86e16c6b61d9dfe0c5c35f821e16de4bc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+912dbe82ae0f18702a7df7a9c983c7eb05ee6ec3")]
 [assembly: System.Reflection.AssemblyProductAttribute("learn csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learn csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
