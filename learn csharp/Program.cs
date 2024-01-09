34671e98331eddd6401a75dd7d63ebbaf419795c
Mr.Robot <elisiomassaqui8@gmail.com>
2024-01-09 17:14:47 +0000

    antes de limpo e depois de limpo

--- learn csharp/Program.cs
+++ learn csharp/Program.cs
@@ -184,12 +184,9 @@ Console.WriteLine($"decimal: {myDecimal}");
 string[] pallets = { "B14", "A11", "B12", "A13" };
 Console.WriteLine("");
 
+Console.WriteLine($"Before: {pallets[0]}");
 Array.Clear(pallets, 0, 2);
-Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
+Console.WriteLine($"After: {pallets[0]}");
 
 
 
